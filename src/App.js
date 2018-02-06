413336519e42b2ad1d3ab4138e27ba5aba4de53e
Shean <ys.chong@finexusgroup.com>
2018-02-06 11:05:10 +0800

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,21 @@
+// @flow
+import React from "react";
+import "normalize.css/normalize.css";
+import "./styles/styles.css";
+
+export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    // this.handlePick = this.handlePick.bind(this);
+    this.state = {
+
+    };
+  }
+  render() {
+    return (
+      <div>
+        This is my boilerplate.
+      </div>
+    );
+  }
+}
